f5c21fae21c9073eaebfb6fd99f550de9ef89761
zyf <754254702@qq.com>
2021-04-15 17:09:05 +0800

    第一次提交

---
+++ src/main/java/com/example/demo415/Demo415Application.java
@@ -0,0 +1,13 @@
+package com.example.demo415;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo415Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo415Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo415/Demo415ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo415;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo415ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
